12a35a6ab67495a3dc3bb36111ad65078606bf9a
htwnavarro <herontnavarro@gmail.com>
2024-09-20 20:27:40 -0700

    pull out graph construction

--- src/graph/bfs.ts
+++ src/graph/bfs.ts
@@ -9,39 +9,17 @@
  */
 import {GraphNode} from "./GraphNode";
 import {Queue} from "../queue/Queue";
-
-console.log('Constructing graph:\n')
-console.log(`
- *                A
- *          _ _ _   _ _ _
- *        |               |
- *        B               C
- *      _ _ _           _ _ _
- *    |       |       |       |
- *    D       E       F       G
-`)
-
-const ROOT_NODE = 'A';
-// root node
-const root = new GraphNode(ROOT_NODE);
-
-// tier 1
-const B = root.add('B')
-const C = root.add('C')
-
-// tier 2
-const D = B.add('D')
-const E = B.add('E')
-const F = C.add('F')
-const G = C.add('G')
+import {ROOT, init as initGraph} from "./GraphABC";
 
 const bfs = (searchFor: string | undefined) => {
+    initGraph();
+
     if (searchFor) {
         console.log(`BFS for node: ${searchFor}\n`)
     } else {
-        console.log('Traverse graph Bread-First:\n')
+        console.log('Traverse graph Breadth-First:\n')
     }
-    const q = new Queue<GraphNode<string>>([root])
+    const q = new Queue<GraphNode<string>>([ROOT])
     const vq = new Queue<GraphNode<string>>()
 
     let foundNode: GraphNode<string> | null = null;
